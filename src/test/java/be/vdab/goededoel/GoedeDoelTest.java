99c4da331af6d44f9b3aa5e8084d09a26d8f4e38
Hans Desmet <hans.desmet@vdabcampus.be>
2025-10-27 15:27:14 +0100

    extra code

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -30,6 +30,10 @@ class GoedeDoelTest {
     void doelVerschiltVanEenObjectMetEenAnderType() {
         assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
     }
+    @Test
+    void gelijkeDoelGevenHebbenDezelfdeHashCode() {
+        assertThat(doel).hasSameHashCodeAs(new GoedeDoel(NAAM));
+    }
 
 
 
