b7d576c751b20982524e30b95682c8405cbc785b
Hans Desmet <hans.desmet@vdabcampus.be>
2025-10-27 14:45:38 +0100

    Initial commit

---
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -0,0 +1,18 @@
+package be.vdab.goededoel;
+import java.math.BigDecimal;
+public class GoedeDoel {
+    private final String naam;
+    private BigDecimal opbrengst;
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public BigDecimal getOpbrengst() {
+        return opbrengst;
+    }
+    // getters voor naam en opbrengst
+}
