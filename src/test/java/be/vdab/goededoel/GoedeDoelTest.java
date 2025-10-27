e3e8437f88bae8fc6f9d4c285e3722b07a26594b
Hans Desmet <hans.desmet@vdabcampus.be>
2025-10-27 15:05:09 +0100

    nieuwe test: eenNieuwDoelHeeftNogGeenOpbrengst

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -13,4 +13,9 @@ class GoedeDoelTest {
     void getNaam() {
         assertThat(doel.getNaam()).isEqualTo(NAAM);
     }
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpbrengst()).isZero();
+    }
+
 }
